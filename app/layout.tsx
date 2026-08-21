08b4d4f4226879749ad1c13905b33470c29fb77f
MrDan001 <99513727+MrDan001@users.noreply.github.com>
2026-08-21 10:30:29 +0200

    feat: load app shell styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";
 import "./globals.css";
+import "./home.css";
 import GameSocialOverlay from "./_components/GameSocialOverlay";
 import AppShell from "./AppShell";
 
