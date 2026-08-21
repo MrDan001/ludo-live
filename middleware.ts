f8af98635fabc49518c923fe7f3a1d904b6405ce
MrDan001 <99513727+MrDan001@users.noreply.github.com>
2026-08-21 16:58:24 +0200

    Rename home route to dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-export function middleware(request: NextRequest){const {pathname}=request.nextUrl;if(pathname==="/")return NextResponse.redirect(new URL("/home",request.url));if(pathname==="/game"){const url=request.nextUrl.clone();url.pathname="/";return NextResponse.rewrite(url)}return NextResponse.next()}
-export const config={matcher:["/","/game"]};
+
+export function middleware(request: NextRequest){
+  const {pathname}=request.nextUrl;
+  if(pathname==="/home") return NextResponse.redirect(new URL("/dashboard",request.url));
+  if(pathname==="/dashboard"){
+    const url=request.nextUrl.clone();
+    url.pathname="/home";
+    return NextResponse.rewrite(url);
+  }
+  if(pathname==="/game"){
+    const url=request.nextUrl.clone();
+    url.pathname="/home";
+    return NextResponse.rewrite(url);
+  }
+  return NextResponse.next();
+}
+
+export const config={matcher:["/home","/dashboard","/game"]};
