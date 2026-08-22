bd128e15054203a13f6adf59a6e0333f5e119449
MrDan001 <99513727+MrDan001@users.noreply.github.com>
2026-08-22 22:36:13 +0200

    Stop /game from being rewritten to /board

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,3 @@
 import {NextRequest,NextResponse} from "next/server";
-export function middleware(request:NextRequest){const{pathname}=request.nextUrl;if(pathname==="/home")return NextResponse.redirect(new URL("/dashboard",request.url));if(pathname==="/dashboard"){const url=request.nextUrl.clone();url.pathname="/home";return NextResponse.rewrite(url)}if(pathname==="/game"){const url=request.nextUrl.clone();url.pathname="/board";return NextResponse.rewrite(url)}return NextResponse.next()}
-export const config={matcher:["/home","/dashboard","/game"]};
+export function middleware(request:NextRequest){const{pathname}=request.nextUrl;if(pathname==="/home")return NextResponse.redirect(new URL("/dashboard",request.url));if(pathname==="/dashboard"){const url=request.nextUrl.clone();url.pathname="/home";return NextResponse.rewrite(url)}return NextResponse.next()}
+export const config={matcher:["/home","/dashboard"]};
