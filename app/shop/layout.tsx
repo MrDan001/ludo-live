b7385b68b44bd3995bb65a3aeeea1a5b9f7b5879
MrDan001 <99513727+MrDan001@users.noreply.github.com>
2026-08-22 23:47:27 +0200

    Remove legacy shop DOM sync layers

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -1,13 +1,5 @@
 import type { ReactNode } from "react";
-import ShopTabsLock from "./ShopTabsLock";
-import ShopInventorySync from "./ShopInventorySync";
 
 export default function ShopLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <ShopTabsLock />
-      <ShopInventorySync />
-      {children}
-    </>
-  );
+  return children;
 }
