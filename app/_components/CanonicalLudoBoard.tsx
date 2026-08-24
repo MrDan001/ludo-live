eec320027232449bb709681b9f5901b601d4b82e
MrDan001 <99513727+MrDan001@users.noreply.github.com>
2026-08-24 11:02:57 +0200

    Fix legal-move marker build import

--- app/_components/CanonicalLudoBoard.tsx
+++ app/_components/CanonicalLudoBoard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React from "react";
 import LudoBoard,{BOARD_NAMES,BOARD_PALETTES,type BoardThemeId,type DemoToken} from "./LudoBoard";
 import {getTokenCell} from "../../lib/canonicalLudoBoard";
 export type {BoardThemeId,DemoToken};
