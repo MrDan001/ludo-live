ae53312570d3dc4e3091bd07745c5af5ec33a05f
MrDan001 <99513727+MrDan001@users.noreply.github.com>
2026-09-07 12:41:07 +0200

    Restore standard Capacitor configuration for web app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,14 +4,12 @@ const config: CapacitorConfig = {
   appId: 'live.ludo.app',
   appName: 'Ludo Live',
   webDir: 'public',
-  appendUserAgent: 'LudoLiveApp/1',
   server: {
     url: 'https://ludo-live.up.railway.app',
     cleartext: false
   },
   android: {
-    allowMixedContent: false,
-    appendUserAgent: 'LudoLiveApp/1'
+    allowMixedContent: false
   }
 };
 
